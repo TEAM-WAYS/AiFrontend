cd7c5836e47289cb8993ec791e0ddc2aafe0d20a
yuyi0011 <yuyi0011@stud.kea.dk>
2023-11-06 13:04:25 +0100

    Front page 01

---
+++ FrontPage.js
@@ -0,0 +1,16 @@
+
+    function selectDish() {
+    const cuisine = document.getElementById("cuisine").value;
+    const spiciness = document.querySelector('input[name="spiciness"]:checked').value;
+    const dietOptions = document.querySelectorAll('input[name="diet"]:checked');
+    const diet = Array.from(dietOptions).map(option => option.value);
+    const time=document.getSelection().valueOf("time")
+
+
+    // Perform some logic to choose the dish based on user preferences
+    // You can use if-else statements or a database of recipes here
+
+    // Display the result to the user
+    const resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = `<p>Your perfect dish is: [Replace with the selected dish]</p>`;
+}
