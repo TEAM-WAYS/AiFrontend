888af22114c1d5ee731d274b1d27b4b486c93832
yuyi0011 <yuyi0011@stud.kea.dk>
2023-11-09 17:57:03 +0100

    Creat a new user

---
+++ createUser.js
@@ -0,0 +1,35 @@
+document.getElementById("registration-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
+
+    let user = {
+        username: username,
+        password: password,
+        email: email
+    };
+
+    fetch('/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+        .then(response => {
+            if (response.ok) {
+                // User registration was successful
+                document.getElementById("error-message").style.display = "none";
+                alert("User registered successfully!");
+            } else {
+                // Handle registration errors
+                response.json().then(data => {
+                    document.getElementById("error-message").innerText = data.message;
+                    document.getElementById("error-message").style.display = "block";
+                });
+            }
+        })
+})
+
