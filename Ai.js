3f622dd2c3d4f999fe787dfc024f25e16b986e23
Andsemandse <andr90m3@stud.kea.dk>
2023-11-09 12:11:30 +0100

    BMI

--- Ai.js
+++ Ai.js
@@ -74,3 +74,36 @@ document.getElementById("people").addEventListener("change", function() {
     }
 });
 
+document.getElementById("bmiCalculatorForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const height = parseFloat(document.getElementById("height").value);
+    const weight = parseFloat(document.getElementById("weight").value);
+
+    if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
+        document.getElementById("bmiResult").textContent = "Invalid input. Please enter valid height and weight.";
+        return;
+    }
+
+    const bmi = calculateBMI(height, weight);
+    const bmiCategory = getBMICategory(bmi);
+
+    document.getElementById("bmiResult").textContent = `Your BMI: ${bmi.toFixed(2)} (${bmiCategory})`;
+});
+
+function calculateBMI(height, weight) {
+    // BMI formula: weight (kg) / (height (m) * height (m))
+    return weight / ((height / 100) * (height / 100));
+}
+
+function getBMICategory(bmi) {
+    if (bmi < 18.5) {
+        return "Underweight";
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        return "Normal Weight";
+    } else if (bmi >= 25 && bmi < 29.9) {
+        return "Overweight";
+    } else {
+        return "Obese";
+    }
+}
