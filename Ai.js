e6cbef51396ec3c76dc42af82f706f887cb49f1e
Andsemandse <andr90m3@stud.kea.dk>
2023-11-10 12:07:09 +0100

    BMi til DEVELOPER

--- Ai.js
+++ Ai.js
@@ -14,6 +14,20 @@ document.getElementById("dishSelectorForm").addEventListener("submit", function 
     const people = document.getElementById("people").value;
     const otherCuisine = document.getElementById("otherCuisine").value;
     const otherPeople = document.getElementById("otherPeople").value;
+    const height = parseFloat(heightInput.value);
+    const weight = parseFloat(weightInput.value);
+
+
+    if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
+        document.getElementById("bmiResult").textContent = "Invalid input. Please enter valid height and weight.";
+        return;
+    }
+
+
+
+    const bmi = calculateBMI(height, weight);
+    const bmiCategory = getBMICategory(bmi);
+
 
     let recommendation = "Create a JSON object with field names ‘NAME’, ‘INGREDIENTS’ and ‘INSTRUCTIONS’";
     recommendation += `assign dish name to ‘NAME’, assign ingredients for the dish to ‘INGREDIENTS’ and cooking instructions to ‘INSTRUCTIONS’`;
@@ -23,6 +37,10 @@ document.getElementById("dishSelectorForm").addEventListener("submit", function 
     recommendation += `3. Dietary Preferences: ${dietaryPreferences.join(", ")}`;
     recommendation += `4. Cooking Time: ${time}`;
     recommendation += `5. Number of People: ${otherPeople || people}`;
+    recommendation += `6. BMI Category: ${bmiCategory}`;
+    if (bmiCategory === "Overweight" || bmiCategory === "Obese") {
+        recommendation += ` (Healthy Food)`;
+    }
     console.log(recommendation)
     console.log("-----------")
     fetchDishRecommendation(recommendation)
@@ -70,4 +88,51 @@ document.getElementById("people").addEventListener("change", function() {
         otherPeopleInput.value = "";
     }
 });
+
+
+document.getElementById("bmiCalculatorForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const heightInput = document.getElementById("height");
+    const weightInput = document.getElementById("weight");
+    const bmiResultElement = document.getElementById("bmiResult");
+
+
+    const height = parseFloat(heightInput.value);
+    const weight = parseFloat(weightInput.value);
+
+    if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
+        bmiResultElement.textContent = "Invalid input. Please enter valid height and weight.";
+        return;
+    }
+
+
+
+    const bmi = calculateBMI(height, weight);
+    const bmiCategory = getBMICategory(bmi);
+
+
+
+
+
+    bmiResultElement.textContent = `Your BMI: ${bmi.toFixed(2)} (${bmiCategory})`;
+});
+
+function calculateBMI(height, weight) {
+    // BMI formula: weight (kg) / (height (m) * height (m))
+    return weight / ((height / 100) * (height / 100));
+}
+
+function getBMICategory(bmi) {
+    if (bmi < 18.5) {
+        return "Underweight";
+    } else if (bmi < 24.9) {
+        return "Normal Weight";
+    } else if (bmi < 29.9) {
+        return "Overweight";
+    } else {
+        return "Obese";
+    }
+}
+
 
